bb1ca7dfb021f57ac1b9f8dce53ef41c8338fa62
prabhath191006 <192411198.simats@saveetha.com>
2025-05-04 09:27:55 +0530

    Update ProcessCreation.c

--- ProcessCreation.c
+++ ProcessCreation.c
@@ -1,28 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
 
 int main() {
-    pid_t pid;
+    int pid = fork();  // Create a new process
 
-    printf("Before fork: This is the main process.\n");
-
-    pid = fork();  // Create a new process
-
-    if (pid < 0) {
-        // Fork failed
-        perror("Fork failed");
-        return 1;
-    } else if (pid == 0) {
+    if (pid == 0) {
         // Child process
-        printf("Child process:\n");
-        printf("  PID: %d\n", getpid());
-        printf("  Parent PID: %d\n", getppid());
+        printf("This is the Child Process.\n");
     } else {
         // Parent process
-        printf("Parent process:\n");
-        printf("  PID: %d\n", getpid());
-        printf("  Child PID: %d\n", pid);
+        printf("This is the Parent Process.\n");
     }
 
     return 0;
