355aa5925f2b0faba2e16a173e996b72e7fc59d7
prabhath191006 <192411198.simats@saveetha.com>
2025-05-28 06:46:31 +0530

    17- First Come First Served disk scheduling.c

--- 17- First Come First Served disk scheduling.c
+++ 17- First Come First Served disk scheduling.c
@@ -1,17 +1,31 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
-  int ReadyQueue[100],i,n,TotalHeadMov=0,initial;
-  scanf("%d",&n);
-  for(i=0;i<n;i++){
-  scanf("%d",&ReadyQueue[i]);
-  }
-  scanf("%d",&initial);
-  for(i=0;i<n;i++)
-  {
-    TotalHeadMov=TotalHeadMov+abs(ReadyQueue[i]-initial);
-    initial=ReadyQueue[i];
-  }
-  printf("Total Head Movement=%d",TotalHeadMov);
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int ReadyQueue[100], i, n, TotalHeadMov = 0, initial;
+
+    // Input: Number of disk requests
+    printf("Enter the number of requests: ");
+    scanf("%d", &n);
+
+    // Input: The disk queue
+    printf("Enter the disk queue: ");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &ReadyQueue[i]);
+    }
+
+    // Input: Initial head position
+    printf("Enter the initial head position: ");
+    scanf("%d", &initial);
+
+    // FCFS disk scheduling calculation
+    for(i = 0; i < n; i++) {
+        TotalHeadMov += abs(ReadyQueue[i] - initial);
+        initial = ReadyQueue[i];
+    }
+
+    // Output total head movement
+    printf("Total Head Movement = %d\n", TotalHeadMov);
+
+    return 0;
 }
